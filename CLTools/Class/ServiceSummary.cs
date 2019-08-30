c79819b48edfd112cb6280b5f184340719648ffe
tgiqfe <tqji31001@gmail.com>
2019-08-30 20:46:00 +0900

    Start EasyService

---
+++ CLTools/Class/ServiceSummary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLTools.Class
+{
+    public class ServiceSummary
+    {
+        public enum ServiceStartMode { Automatic, Manual, Disabled, DelayedAutomatic };
+
+        public string ServiceName { get; set; }
+        public string DisplayName { get; set; }
+
+    }
+}
