600e7455983c7c96b06b58bf621bede50ccfd288
tgiqfe <tqji31001@gmail.com>
2019-08-30 23:03:16 +0900

    Mod Set-WindowsService

---
+++ CLTools/Class/ServiceControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace CLTools.Class
+{
+    class ServiceControl
+    {
+        /// <summary>
+        /// サービス名からServiceControllerを取得
+        /// </summary>
+        /// <param name="serviceName">ServiceName, DisplayNameどちらでも可。ServiceName優先</param>
+        /// <returns>ServiceControllerインスタンス。指定した名前に一致したサービスが無い場合はnull</returns>
+        public static ServiceController GetServiceController(string serviceName)
+        {
+            ServiceController retSV = ServiceController.GetServices().FirstOrDefault(
+                x => x.ServiceName.Equals(serviceName, StringComparison.OrdinalIgnoreCase));
+            if (retSV == null)
+            {
+                retSV = ServiceController.GetServices().FirstOrDefault(
+                    x => x.DisplayName.Equals(serviceName, StringComparison.OrdinalIgnoreCase));
+            }
+            return retSV;
+        }
+    }
+}
---
+++ CLTools/Cmdlet/SetWindowsService.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+using CLTools.Class;
+using System.ComponentModel;
+
+namespace CLTools.Cmdlet
+{
+    [Cmdlet(VerbsCommon.Set, "WindowsService")]
+    public class SetWindowsService : PSCmdlet
+    {
+        #region SetStartupType
+        [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern Boolean ChangeServiceConfig(
+            IntPtr hService,
+            uint nServiceType,
+            uint nStartType,
+            uint nErrorControl,
+            string lpBinaryPathName,
+            string lpLoadOrderGroup,
+            IntPtr lpdwTagId,
+            [In] char[] lpDependencies,
+            string lpServiceStartName,
+            string lpPassword,
+            string lpDisplayName);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ChangeServiceConfig2(IntPtr hService, int dwInfoLevel, IntPtr lpInfo);
+
+        private const uint SERVICE_NO_CHANGE = 0xFFFFFFFF;
+        private const uint SERVICE_QUERY_CONFIG = 0x00000001;
+        private const uint SERVICE_CHANGE_CONFIG = 0x00000002;
+        private const uint SC_MANAGER_ALL_ACCESS = 0x000F003F;
+        private const int SERVICE_CONFIG_DELAYED_AUTO_START_INFO = 0x00000003;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct SERVICE_DELAYED_AUTO_START_INFO { public bool fDelayedAutostart; }
+
+        [DllImport("advapi32.dll", EntryPoint = "CloseServiceHandle")]
+        private static extern int CloseServiceHandle(IntPtr hSCObject);
+
+        private static void ThrowLastWin32Error(string messagePrefix)
+        {
+            int nError = Marshal.GetLastWin32Error();
+            var win32Exception = new Win32Exception(nError);
+            string message = string.Format("{0}: {1}", messagePrefix, win32Exception.Message);
+            throw new ExternalException(message);
+        }
+
+        private static IntPtr OpenServiceManagerHandle()
+        {
+            IntPtr serviceManagerHandle = OpenSCManager(null, null, SC_MANAGER_ALL_ACCESS);
+            if (serviceManagerHandle == IntPtr.Zero)
+            {
+                throw new ExternalException("サービスマネージャのオープンに失敗");
+            }
+            return serviceManagerHandle;
+        }
+        private static IntPtr OpenServiceHandle(ServiceController serviceController, IntPtr serviceManagerHandle)
+        {
+            var serviceHandle = OpenService(
+                serviceManagerHandle, serviceController.ServiceName, SERVICE_QUERY_CONFIG | SERVICE_CHANGE_CONFIG);
+            if (serviceHandle == IntPtr.Zero)
+            {
+                throw new ExternalException("サービスのオープンに失敗");
+            }
+            return serviceHandle;
+        }
+        //private static void ChangeServiceStartType(IntPtr serviceHandle, ServiceStartMode mode)
+        private static void ChangeServiceStartType(IntPtr serviceHandle, string startMode)
+        {
+            uint mode = 0;
+            switch (startMode)
+            {
+                case AUTOMATIC: mode = 2; break;
+                case MANUAL: mode = 3; break;
+                case DISABLED: mode = 4; break;
+            }
+            bool ret = ChangeServiceConfig(
+                serviceHandle,
+                SERVICE_NO_CHANGE,
+                mode,
+                SERVICE_NO_CHANGE,
+                null,
+                null,
+                IntPtr.Zero,
+                null,
+                null,
+                null,
+                null);
+            if (!ret)
+            {
+                ThrowLastWin32Error("サービス開始タイプの変更に失敗");
+            }
+        }
+        private static void ChangeDelayedAutoStart(IntPtr hService, bool delayed)
+        {
+            SERVICE_DELAYED_AUTO_START_INFO info = new SERVICE_DELAYED_AUTO_START_INFO();
+            info.fDelayedAutostart = delayed;
+            IntPtr hInfo = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(SERVICE_DELAYED_AUTO_START_INFO)));
+            Marshal.StructureToPtr(info, hInfo, true);
+            bool ret = ChangeServiceConfig2(hService, SERVICE_CONFIG_DELAYED_AUTO_START_INFO, hInfo);
+            Marshal.FreeHGlobal(hInfo);
+            if (!ret)
+            {
+                ThrowLastWin32Error("サービス遅延自動設定の変更に失敗");
+            }
+        }
+        #endregion
+
+        const string NONE = "None";
+        const string AUTOMATIC = "Automatic";
+        const string MANUAL = "Manual";
+        const string DISABLED = "Disabled";
+        const string DELAYED_AUTOMATIC = "DelayedAutomatic";
+        //enum ServiceStartMode { Automatic = 2, Manual = 3, Disabled = 4, DelayedAutomatic = 99 };
+
+
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Service { get; set; }
+        [Parameter]
+        [ValidateSet(NONE, AUTOMATIC, MANUAL, DISABLED, DELAYED_AUTOMATIC)]
+        public string StartupType { get; set; } = NONE;
+
+        protected override void ProcessRecord()
+        {
+            ServiceController serviceController = ServiceControl.GetServiceController(Service);
+
+            IntPtr serviceManagerHandle = OpenServiceManagerHandle();
+            IntPtr serviceHandle = OpenServiceHandle(serviceController, serviceManagerHandle);
+
+            try
+            {
+                if (StartupType == DELAYED_AUTOMATIC)
+                {
+                    ChangeServiceStartType(serviceHandle, AUTOMATIC);
+                    ChangeDelayedAutoStart(serviceHandle, true);
+                }
+                else
+                {
+                    ChangeDelayedAutoStart(serviceHandle, false);
+                    ChangeServiceStartType(serviceHandle, StartupType);
+                }
+
+                /*
+                switch (StartupType)
+                {
+                    case DELAYED_AUTOMATIC:
+                        ChangeServiceStartType(serviceHandle, ServiceStartMode.Automatic);
+                        ChangeDelayedAutoStart(serviceHandle, true);
+                        break;
+                    case AUTOMATIC:
+                        ChangeDelayedAutoStart(serviceHandle, false);
+                        ChangeServiceStartType(serviceHandle, ServiceStartMode.Automatic);
+                        break;
+                    case MANUAL:
+                        ChangeDelayedAutoStart(serviceHandle, false);
+                        ChangeServiceStartType(serviceHandle, ServiceStartMode.Manual);
+                        break;
+                    case DISABLED:
+                        ChangeDelayedAutoStart(serviceHandle, false);
+                        ChangeServiceStartType(serviceHandle, ServiceStartMode.Disabled);
+                        break;
+                }
+                */
+            }
+            catch { }
+            if (serviceHandle != IntPtr.Zero)
+            {
+                CloseServiceHandle(serviceHandle);
+            }
+            if (serviceHandle != IntPtr.Zero)
+            {
+                CloseServiceHandle(serviceManagerHandle);
+            }
+        }
+    }
+}
