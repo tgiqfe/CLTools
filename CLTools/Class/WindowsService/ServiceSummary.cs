3eb3683cc582a111b8efeb633b42231323a91635
tgiqfe <tqji31001@gmail.com>
2019-08-31 09:44:06 +0900

    Mod ServiceSummary

--- CLTools/Class/WindowsService/ServiceSummary.cs
+++ CLTools/Class/WindowsService/ServiceSummary.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ServiceProcess;
 using Microsoft.Win32;
+using System.Management;
 
 namespace CLTools.Class
 {
@@ -12,20 +13,39 @@ namespace CLTools.Class
     {
         public string Name { get; set; }
         public string DisplayName { get; set; }
+        public string Status { get; set; }
+
         public string StartupType { get; set; }
         public string[] ServicesDependedOn { get; set; }
         public string[] DependentServices { get; set; }
+        public string ExecutePath { get; set; }
+        public string Description { get; set; }
+        public string LogonName { get; set; }
+        public int? ProcessId { get; set; }
 
         public ServiceSummary() { }
-        public ServiceSummary(string name)
+        public ServiceSummary(ServiceController sc)
         {
-            ServiceController sc = ServiceControl.GetServiceController(name);
-            this.Name = sc.ServiceName;
-            this.DisplayName = sc.DisplayName;
-
-            this.StartupType = sc.StartType.ToString();
-            if (sc.StartType == ServiceStartMode.Automatic)
+            if (sc != null)
             {
+                ManagementObject mo = new ManagementClass("Win32_Service").
+                    GetInstances().
+                    OfType<ManagementObject>().
+                    FirstOrDefault(x =>
+                        x["Name"] != null &&
+                        (x["Name"] as string).Equals(sc.ServiceName, StringComparison.OrdinalIgnoreCase));
+
+                this.Name = sc.ServiceName;
+                this.DisplayName = sc.DisplayName;
+                this.Status = sc.Status.ToString();
+
+                this.StartupType = sc.StartType.ToString();
+                if (sc.StartType == ServiceStartMode.Automatic && (bool)mo["DelayedAutoStart"])
+                {
+                    this.StartupType = "DelayedAutomatic";
+                }
+                //  Win32APIからのほうがスマートなので、レジストリから調べる案は廃止
+                /*
                 int delayedAutoStart = (int)Registry.GetValue(
                     @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\" + sc.ServiceName,
                     "DelayedAutostart", 0);
@@ -33,12 +53,16 @@ namespace CLTools.Class
                 {
                     this.StartupType = "DelayedAutomatic";
                 }
+                */
+
+                this.ServicesDependedOn = sc.ServicesDependedOn.Select(x => x.ServiceName).ToArray();
+                this.DependentServices = sc.DependentServices.Select(x => x.ServiceName).ToArray();
+
+                this.ExecutePath = mo["PathName"] as string;
+                this.Description = mo["Description"] as string;
+                this.LogonName = mo["StartName"] as string;
+                this.ProcessId = mo["ProcessId"] as int?;
             }
-
-            this.ServicesDependedOn = sc.ServicesDependedOn.Select(x => x.ServiceName).ToArray();
-            this.DependentServices = sc.DependentServices.Select(x => x.ServiceName).ToArray();
-
         }
-
     }
 }
