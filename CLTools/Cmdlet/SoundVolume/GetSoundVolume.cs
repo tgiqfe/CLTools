0f904f1e59052dc5e3159b9765179d4d1c1c7b31
tgiqfe <tqji31001@gmail.com>
2019-08-31 13:31:05 +0900

    Add Get-SoundVolume

---
+++ CLTools/Cmdlet/SoundVolume/GetSoundVolume.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using CLTools.Class;
+
+namespace CLTools.Cmdlet
+{
+    [Cmdlet(VerbsCommon.Get, "SoundVolume")]
+    public class GetSoundVolume : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject(new VolumeSummary());
+        }
+    }
+}
