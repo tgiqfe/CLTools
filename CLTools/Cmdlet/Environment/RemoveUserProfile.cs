42e22431588ed9456c5fdf472329b0423e02c9a0
tgiqfe <tqji31001@gmail.com>
2019-10-09 21:22:04 +0900

    Add Remove-UserProfile

---
+++ CLTools/Cmdlet/Environment/RemoveUserProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management;
+
+namespace CLTools.Cmdlet
+{
+    [Cmdlet(VerbsCommon.Remove, "UserProfile")]
+    public class RemoveUserProfile : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string Name { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            UserSummary user = new UserSummary(Name);
+            ManagementObject mo = new ManagementClass("Win32_UserProfile").
+                GetInstances().
+                OfType<ManagementObject>().
+                FirstOrDefault(x => x["SID"].ToString().Equals(user.SID, StringComparison.OrdinalIgnoreCase));
+            if (mo != null)
+            {
+                mo.Delete();
+            }
+        }
+    }
+}
