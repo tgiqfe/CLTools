e55cdac1ebead2ce8fdfe7b3304eee6f659e5573
tgiqfe <tqji31001@gmail.com>
2020-09-13 21:12:03 +0900

    Add Set-CLMachineName cmdlet.

---
+++ CLTools/Cmdlet/Environment/SetCLMachineName.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Diagnostics;
+
+namespace CLTools.Cmdlet.Environment
+{
+    /// <summary>
+    /// ホスト名を変更する
+    /// </summary>
+    [Cmdlet(VerbsCommon.Set, "CLMachineName")]
+    public class SetCLMachineName : PSCmdlet
+    {
+        [Parameter]
+        public string Name { get; set; }
+        [Parameter]
+        public bool Restart { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            bool ret = Functions.CheckAdmin();
+            if (!ret) { return; }
+
+            if (!string.IsNullOrEmpty(Name) &&
+                !Name.Equals(System.Environment.MachineName, StringComparison.OrdinalIgnoreCase))
+            {
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo.FileName = "cmd.exe";
+                    proc.StartInfo.Arguments =
+                        $"/c wmic computersystem where name=\"%computername%\" call rename name=\"{Name}\"";
+                    proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    proc.Start();
+                    proc.WaitForExit();
+                }
+
+                if (Restart)
+                {
+                    using (Process proc = new Process())
+                    {
+                        proc.StartInfo.FileName = "cmd.exe";
+                        proc.StartInfo.Arguments =
+                            $"/c shutdown /r /t 0";
+                        proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        proc.Start();
+                    }
+                }
+            }
+        }
+    }
+}
